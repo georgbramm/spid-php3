191ef92d247ad3887455b8fd809c4d44f88ee64d
Lorenzo Cattaneo <development@lorenzocattaneo.com>
2018-08-10 01:06:46 +0200

    load idp meta

--- src/Spid/Saml.php
+++ src/Spid/Saml.php
@@ -10,7 +10,21 @@ class Saml implements SpInterface
 
     public function loadIdpMetadata($path){}
 
-    public function loadIdpFromFile($filename){}
+    public function loadIdpFromFile($filename){
+        if (!file_exists(Constants::APP_PATH . $folder . $filename . ".xml")) {
+            throw new \Exception("Invalid IDP Requested", 1);
+        }
+        
+        $xml = simplexml_load_file(Constants::APP_PATH . $filename . '.xml');
+        
+        $metadata = array();
+        $metadata['idpEntityId'] = $xml->attributes()->entityID->__toString();
+        $metadata['idpSSO'] = $xml->xpath('//SingleSignOnService')[0]->attributes()->Location->__toString();
+        $metadata['idpSLO'] = $xml->xpath('//SingleLogoutService')[0]->attributes()->Location->__toString();
+        $metadata['idpCertValue'] = $xml->xpath('//X509Certificate')[0]->__toString();
+         
+        return $metadata;
+    }
 
     public function getSPMetadata(){}
 
